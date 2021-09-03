a70a8fb19694d5b2193b8dcdb2ebf7641565e5fb
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-09-03 00:41:05 +0000

    #5 Add representation of antenna log

--- aste_xffts_merge/antenna.py
+++ aste_xffts_merge/antenna.py
@@ -3,14 +3,15 @@
 
 # standard library
 from dataclasses import dataclass
+from typing import Tuple
 
 
 # dependencies
-from xarray_dataclasses import Attr, Data, Name
+from xarray_dataclasses import AsDataset, Attr, Coordof, Data, Dataof, Name
 
 
 # submodules
-from common import DEFAULT_FLOAT, readonly, Time
+from common import DEFAULT_FLOAT, DEFAULT_TIME, readonly, Time, TimeAxis
 
 
 # dataclasses
@@ -48,3 +49,32 @@ class Latitude:
     data: Data[Time, float] = DEFAULT_FLOAT
     name: Name[str] = readonly("Latitude")
     units: Attr[str] = readonly("degree")
+
+
+@dataclass
+class Antenna(AsDataset):
+    """Representation of antenna log."""
+
+    azimuth: Dataof[Azimuth] = DEFAULT_FLOAT
+    """Antenna azimuth (in degree)."""
+
+    elevation: Dataof[Elevation] = DEFAULT_FLOAT
+    """Antenna elevation (in degree)."""
+
+    longitude: Dataof[Longitude] = DEFAULT_FLOAT
+    """Sky latitude (in degree)."""
+
+    latitude: Dataof[Latitude] = DEFAULT_FLOAT
+    """Sky latitude (in degree)."""
+
+    ref_longitude: Data[Tuple[()], float] = DEFAULT_FLOAT
+    """Reference sky longitude (in degree)."""
+
+    ref_latitude: Data[Tuple[()], float] = DEFAULT_FLOAT
+    """Reference sky latitude (in degree)."""
+
+    frame: Data[Tuple[()], str] = "ICRS"
+    """Frame of sky coordinates."""
+
+    t: Coordof[TimeAxis] = DEFAULT_TIME
+    """Observed time (in UTC)."""
