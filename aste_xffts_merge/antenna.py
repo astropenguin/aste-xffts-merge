77dbaf21c7861a0f2800be88e50f8ad7fa6e7ca3
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-09-03 00:33:41 +0000

    #5 Add representations of lon and lat

--- aste_xffts_merge/antenna.py
+++ aste_xffts_merge/antenna.py
@@ -16,7 +16,7 @@ from common import DEFAULT_FLOAT, readonly, Time
 # dataclasses
 @dataclass
 class Azimuth:
-    """Representation of azimuth."""
+    """Representation of antenna azimuth."""
 
     data: Data[Time, float] = DEFAULT_FLOAT
     name: Name[str] = readonly("Azimuth")
@@ -25,8 +25,26 @@ class Azimuth:
 
 @dataclass
 class Elevation:
-    """Representation of elevation."""
+    """Representation of antenna elevation."""
 
     data: Data[Time, float] = DEFAULT_FLOAT
     name: Name[str] = readonly("Elevation")
+    units: Attr[str] = readonly("degree")
+
+
+@dataclass
+class Longitude:
+    """Representation of sky longitude."""
+
+    data: Data[Time, float] = DEFAULT_FLOAT
+    name: Name[str] = readonly("Longitude")
+    units: Attr[str] = readonly("degree")
+
+
+@dataclass
+class Latitude:
+    """Representation of sky latitude."""
+
+    data: Data[Time, float] = DEFAULT_FLOAT
+    name: Name[str] = readonly("Latitude")
     units: Attr[str] = readonly("degree")
