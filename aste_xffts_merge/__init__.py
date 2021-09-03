99cee1d27de1f5145e3f00c5ece8871b29959c06
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-09-03 07:17:58 +0000

    #5 Update tests for metadata

--- aste_xffts_merge/__init__.py
+++ aste_xffts_merge/__init__.py
@@ -1,4 +1,5 @@
 # flake8: noqa
+__author__ = "Akio Taniguchi"
 __version__ = "0.1.0"
 
 
--- tests/test_init.py
+++
@@ -1,5 +0,0 @@
-import aste_xffts_merge as merge
-
-
-def test_version() -> None:
-    assert merge.__version__ == "0.1.0"
